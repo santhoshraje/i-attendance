02dde9e21063e81b74b56b7338ade1c9595b9533
Santhoshraj <santhoshraje95@gmail.com>
2020-08-08 22:03:47 +0800

    added dynamic viewing of student's names on the attendance list
    - student's names can now be seen on the attendance list
    - file io added to read class name for later use

--- lib/lecturer/lecturer_view_students.dart
+++ lib/lecturer/lecturer_view_students.dart
@@ -1,8 +1,79 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:iattendance/lecturer/lecturer_send_attendance.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
 
-class ViewStudents extends StatelessWidget {
+class ViewStudents extends StatefulWidget {
+  @override
+  _ViewStudentsState createState() => _ViewStudentsState();
+}
+
+class _ViewStudentsState extends State<ViewStudents> {
+  final List<Map<String, String>> studentList = [];
+
+  // file io functions
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    return directory.path;
+  }
+
+  Future<File> _localFile(String filePath) async {
+    final path = await _localPath;
+    return File('$path/' + filePath);
+  }
+
+  Future<String> readFromDevice(String filePath) async {
+    try {
+      final file = await _localFile(filePath);
+      // Read the file.
+      String contents = await file.readAsString();
+      return contents;
+    } catch (e) {
+      // If encountering an error, return 0.
+      return 'error';
+    }
+  }
+
+  void doStuff() async {
+    var className = await readFromDevice('class_name.txt');
+    // get class name from firebase
+    QuerySnapshot qs;
+    var documentID = '';
+    try {
+      qs = await Firestore.instance.collection('beacons').getDocuments();
+    } catch (e) {
+      print(e.toString());
+    }
+    for (var i = 0; i < qs.documents.length; i++) {
+      if (qs.documents[i].data['class'] == className) {
+        documentID = qs.documents[i].documentID;
+      }
+    }
+    Firestore.instance
+        .collection('beacons')
+        .document(documentID)
+        .snapshots()
+        .listen((DocumentSnapshot documentSnapshot) {
+          var tmp = documentSnapshot.data.keys.toList();
+          tmp.remove('id');
+          tmp.remove('class');
+          studentList.clear();
+          tmp.forEach((element) {
+            studentList.add({"Name": element});
+
+          });
+      setState(() {});
+    });
+  }
+
+  @override
+  void initState() {
+    doStuff();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,13 +93,15 @@ class ViewStudents extends StatelessWidget {
             children: <Widget>[
               Padding(
                 padding: const EdgeInsets.only(top: 20, bottom: 20, left: 20),
-                child: Align(alignment:Alignment.topLeft,child: Text('Attendance count: 39')),
+                child: Align(
+                    alignment: Alignment.topLeft,
+                    child: Text('Attendance count: ' + studentList.length.toString())),
               ),
               Expanded(
                 child: Align(
                   alignment: Alignment.topCenter,
-                  child: DataTable(
-
+                  child:
+                  DataTable(
                     columns: <DataColumn>[
                       DataColumn(
                         label: Text(
@@ -36,34 +109,17 @@ class ViewStudents extends StatelessWidget {
                         ),
                       ),
                     ],
-                    rows: <DataRow>[
-                      DataRow(
-                        cells: <DataCell>[
-                          DataCell(Text('Sarah')),
-                        ],
-                      ),
-                      DataRow(
-                        cells: <DataCell>[
-                          DataCell(Text('Tom')),
-                        ],
-                      ),
-                      DataRow(
-                        cells: <DataCell>[
-                          DataCell(Text('Paul')),
-                        ],
-                      ),
-                      DataRow(
-                        cells: <DataCell>[
-                          DataCell(Text('Sam')),
-                        ],
-                      ),
-                      DataRow(
-                        cells: <DataCell>[
-                          DataCell(Text('James')),
-                        ],
-                      ),
-
-                    ],
+                    rows:
+                        studentList // Loops through dataColumnText, each iteration assigning the value to element
+                            .map(
+                              ((element) => DataRow(
+                                    cells: <DataCell>[
+                                      DataCell(Text(element[
+                                          "Name"])), //Extracting from Map element the value
+                                    ],
+                                  )),
+                            )
+                            .toList(),
                   ),
                 ),
               ),
